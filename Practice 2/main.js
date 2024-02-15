8ee79b31d855dd755f8eff4e124c93daf577bdda
Emirlord13@gmail.com <Emirlord13@gmail.com>
2024-02-15 13:05:24 -0500

    Practice 2

---
+++ Practice 2/main.js
@@ -0,0 +1,25 @@
+// var a = 10
+// var a = 20
+console.log(a)
+var a
+a = ''
+// Var has function scope, you can redeclare a var, vars can be hoisted
+
+E()
+
+// console.log(b)
+let b = 10
+// let b = 20
+// Let has block scope, you can't redeclare a let, vars cannot be hoisted
+
+// const c = 10
+
+function E(){
+// var a = 30
+// console.log(a)
+}
+// E()
+
+// console.log(a)
+
+// Hoisted: a property of JS by which the vars and functions are moved to the top of the scope, values won't be hoisted
